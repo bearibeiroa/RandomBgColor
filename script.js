99b14e0f3e6ef5da3dd8e28c83bd719772ae0caa
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-08-29 05:02:42 +0100

    update: readme | refat script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const button = document.querySelector('#submit');
 
-function randomColor () {
-	const r = Math.floor(Math.random()*256);
-	const g = Math.floor(Math.random()*256);
-	const b = Math.floor(Math.random()*256);
-	const bgColor = "rgb(" + r + "," + g + "," + b + ")";
-	document.body.style.background = bgColor;
+/* The way I already knew
+// function randomColor () {
+// 	const r = Math.floor(Math.random()*256);
+// 	const g = Math.floor(Math.random()*256);
+// 	const b = Math.floor(Math.random()*256);
+// 	const bgColor = "rgb(" + r + "," + g + "," + b + ")";
+// 	document.body.style.background = bgColor;
+// }
+*/
+
+/* The way I learned with this project */
+function randomColorGenerator() {
+  let code = '0123456789ABCDEF';
+  let colorMake = '#';
+
+  for (let index = 0; index < 6; index += 1) {
+    colorMake += code[Math.floor(Math.random() * 16)];
+  }
+  document.body.style.background = colorMake;
 }
 
-
-button.addEventListener('click', randomColor)
+button.addEventListener('click', randomColorGenerator)
